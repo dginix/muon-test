ce005e8fb6841d9762780c0bb8711ba1db701ddc
dginix <kruchininds@yandex.ru>
2023-03-06 19:48:06 +0500

    feat: repository for garage and car entity

---
+++ src/main/java/com/example/muontest/repository/GarageRepository.java
@@ -0,0 +1,10 @@
+package com.example.muontest.repository;
+
+import com.example.muontest.model.Garage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GarageRepository extends JpaRepository<Garage, Long> {
+
+}
