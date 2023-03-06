1617bc939c53c17488b29a6033f7d3069db16c33
dginix <kruchininds@yandex.ru>
2023-03-06 14:02:17 +0500

    Initial commit

---
+++ src/main/java/com/example/muontest/MuonTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.muontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuonTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/muontest/MuonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuonTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
