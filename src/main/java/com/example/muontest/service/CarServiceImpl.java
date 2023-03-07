4d7ca5b8102785c8b693dd485f44cc0601b32c93
dginix <kruchininds@yandex.ru>
2023-03-07 21:54:59 +0500

    feat: throw exception if garage not existed

--- src/main/java/com/example/muontest/service/CarServiceImpl.java
+++ src/main/java/com/example/muontest/service/CarServiceImpl.java
@@ -1,20 +1,26 @@
 package com.example.muontest.service;
 
+import com.example.muontest.exception.NoSuchElementFoundException;
 import com.example.muontest.model.Car;
 import com.example.muontest.repository.CarRepository;
+import com.example.muontest.repository.GarageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CarServiceImpl implements CarService {
 
     private final CarRepository carRepository;
+    private final GarageRepository garageRepository;
 
     @Autowired
-    public CarServiceImpl(CarRepository carRepository) {
+    public CarServiceImpl(CarRepository carRepository,
+                          GarageRepository garageRepository) {
         this.carRepository = carRepository;
+        this.garageRepository = garageRepository;
     }
 
     @Override
@@ -29,6 +35,11 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public Car addCar(Car car) {
-        return carRepository.save(car);
+        if (garageRepository.existsById(car.getGarage().getId())) {
+            return carRepository.save(car);
+        }
+        else {
+            throw new NoSuchElementFoundException("Garage with this number does not exist");
+        }
     }
 }
