219b1ac8394ee4bb85b7de5514b44c1a9d05a6e1
dginix <kruchininds@yandex.ru>
2023-03-08 14:39:30 +0500

    feat: car service возвращает значения вместе с параметрами из вложенного гаража

--- src/main/java/com/example/muontest/service/CarServiceImpl.java
+++ src/main/java/com/example/muontest/service/CarServiceImpl.java
@@ -3,14 +3,13 @@ package com.example.muontest.service;
 import com.example.muontest.exception.GarageCapacityException;
 import com.example.muontest.exception.NoSuchElementFoundException;
 import com.example.muontest.model.Car;
+import com.example.muontest.model.Garage;
 import com.example.muontest.repository.CarRepository;
 import com.example.muontest.repository.GarageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 public class CarServiceImpl implements CarService {
@@ -40,17 +39,17 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public Car addCar(Car car) {
-        Long thisCarGarageId = car.getGarage().getId();
-        if (garageRepository.existsById(thisCarGarageId)) {
-            if (garageRepository.findById(thisCarGarageId).get().getCars().size() >= GARAGE_CAPACITY) {
-                throw new GarageCapacityException("Garage is full");
-            }
-            else {
-                return carRepository.save(car);
-            }
+        Garage thisCarGarage = garageRepository
+                .findById(car.getGarage().getId())
+                .orElseThrow(() -> new NoSuchElementFoundException("Garage with this number does not exist"));
+
+        if (thisCarGarage.getCars().size() >= GARAGE_CAPACITY ) {
+            throw new GarageCapacityException("Garage is full");
         }
         else {
-            throw new NoSuchElementFoundException("Garage with this number does not exist");
+            Car res = carRepository.save(car);
+            res.setGarage(thisCarGarage);
+            return res;
         }
     }
 }
